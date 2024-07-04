193552264e39eed0f56fb8f02049f526b36c8621
Jael <jaelbela36@gmail.com>
2024-07-04 07:58:44 -0300

    first commit

---
+++ practica.py
@@ -0,0 +1,8 @@
+def calculadora (num1, num2):
+    suma = num1 + num2
+    resta= num1 - num2
+    multiplicacion= num1 * num2
+    division = num1 / num2
+    return suma, resta, multiplicacion, division
+
+print(calculadora(3, 4))
